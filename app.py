8a7ed55dbf065a6a490a1d7eb4b9a4b9ad89f371
Emma <emma.tangg@gmail.com>
2023-03-03 08:47:08 -0500

    Cleaned up variables and classes

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def getTracks():
     if os.path.exists(".cache"): 
         os.remove(".cache")
 
-    return render_template('receipt.html', user_display_name=current_user_name, images=images, short_term=short_term, medium_term=medium_term, long_term=long_term, currentTime=gmtime())
+    return render_template('news.html', user_display_name=current_user_name, images=images, short_term=short_term, medium_term=medium_term, long_term=long_term, currentTime=gmtime())
 
 
 @app.template_filter('strftime')
--- setup.py
+++ setup.py
@@ -6,11 +6,9 @@ requires = [
 ]
 
 setup(
-    name='Receiptify',
+    name='Newspaperify',
     version='1.0',
-    description='An application that gets your top spotify tracks and turns them into a receipt',
-    author='Erik Toor',
-    author_email='eriktoor@gmail.com',
+    description='An application that gets your top spotify tracks and turns them into a newspaper based off Receiptify',
     keywords='web flask',
     packages=find_packages(),
     include_package_data=True,
